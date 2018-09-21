62674bf0513e52ea79acce35d5a83fd2843d379f
YONGEEEE <37283217+YONGEEEE@users.noreply.github.com>
2018-09-21 16:46:07 +0900

    Update NetworkTask_AddList.java

--- app/src/main/java/connectDB/NetworkTask_AddList.java
+++ app/src/main/java/connectDB/NetworkTask_AddList.java
@@ -23,7 +23,7 @@ public class NetworkTask_AddList extends AsyncTask<CommentItem, Void, Integer> {
     @Override
     protected Integer doInBackground(CommentItem... voids) {
 
-        String url = "http://203.253.255.113:9090/comment/addList";
+        String url = "http://개인 DB /comment/addList";
         /* Tojson */
 
         String json;
